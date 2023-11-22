65cef56c4bf4d3757569bd3560be7e33f708521a
AndersonLima0 <84778277+AndersonLima0@users.noreply.github.com>
2023-11-22 13:44:34 -0300

    adding collaborators page

--- axios.js
+++ axios.js
@@ -1,30 +1,49 @@
 const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
 
-// Função para fazer a requisição à API da Pokédex usando Axios
 function obterDadosDaAPI() {
-  // Usa o Axios para fazer a requisição
   axios
     .get(apiUrl)
     .then((response) => {
-      // A resposta do Axios é acessada em response.data
       console.log("Dados da API:", response.data);
-      // Chama a função para exibir os dados na página
-      exibirDadosNaPagina(response.data.results);
+      // Limita a exibição para os primeiros 10 pokémons
+      const pokemonsLimitados = response.data.results.slice(0, 10);
+      exibirDadosNaPagina(pokemonsLimitados);
     })
     .catch((erro) => console.error("Erro na requisição:", erro));
 }
 
-// Função para exibir os dados na página
+function obterDetalhesDoPokemon(url) {
+  return axios.get(url);
+}
+
 function exibirDadosNaPagina(pokemons) {
   const outputElement = document.getElementById("output");
   outputElement.innerHTML = "";
 
+  // Itera sobre os pokémons limitados
   pokemons.forEach((pokemon) => {
-    const novoParagrafo = document.createElement("p");
-    novoParagrafo.textContent = `Nome: ${pokemon.name}`;
-    outputElement.appendChild(novoParagrafo);
+    // Para cada Pokémon, faz uma segunda requisição para obter detalhes
+    obterDetalhesDoPokemon(pokemon.url)
+      .then((response) => {
+        const novoDiv = document.createElement("div");
+        novoDiv.classList.add("pokemon-card");
+
+        const nomeParagrafo = document.createElement("p");
+        nomeParagrafo.textContent = `${response.data.name}`;
+
+        const imagem = document.createElement("img");
+        imagem.src = response.data.sprites.front_default;
+        imagem.alt = response.data.name;
+
+        novoDiv.appendChild(nomeParagrafo);
+        novoDiv.appendChild(imagem);
+
+        outputElement.appendChild(novoDiv);
+      })
+      .catch((erro) =>
+        console.error("Erro na requisição de detalhes do Pokémon:", erro)
+      );
   });
 }
 
-// Chama a função para obter dados ao carregar a página
 window.onload = obterDadosDaAPI;
